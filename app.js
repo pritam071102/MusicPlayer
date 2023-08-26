d9094ff401ecf50e0404ffb6e3d554b021fae757
Pritam Kundu <137088791+pritam071102@users.noreply.github.com>
2023-08-26 20:19:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+function f() {
+    window.location.href = "bengali.html";
+}
+function album() {
+    window.location.href = "album.html";
+}
+function montu()
+{
+    window.location.href = "montupilot.html";
+}
+function mohabbatein()
+{
+    window.location.href = "Mohabbatein.html";
+}
+function taqdeer()
+{
+    window.location.href = "taqdeer.html";
+}
+function sad()
+{
+    window.location.href = "sad.html";
+}
+function romantic()
+{
+    window.location.href = "romantic.html";
+}
+function party()
+{
+    window.location.href = "party.html";
+}
+function old_90()
+{
+    window.location.href = "90's.html";
+}
